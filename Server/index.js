2bd824a5815606bc3a8e0668bdb3bcb29cdc1f45
kobi <kobicohencom@gmail.com>
2023-03-25 12:21:15 +0300

    second upload

--- Server/index.js
+++ Server/index.js
@@ -9,19 +9,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-const dataList =[
-    {name:'dani kushmaro',phone:'054444488'},
-    {name:'Meni Mazoz',phone:'0577778889'},
-    {name:'Robin hood',phone:'054144444'},
-
-]
 app.get('/',async(req,res)=>{
     res.json(await mansDb.getData())
 })
 
 app.post('/',async (req,res)=>{
     console.log(req.body)
-    dataList.push(req.body)
     await mansDb.insertDate(req.body)
     res.send('data was great - thanks')
 })
